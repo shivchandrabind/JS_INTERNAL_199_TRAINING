7328267e524a42aa8414fb2820a5b0b012e03249
Shiv Chandra Bind <bdrdshivchandra9125@gmail.com>
2025-12-10 05:15:08 +0000

    array methods commit

---
+++ Day3/p1.js
@@ -0,0 +1,21 @@
+/*function fun(n){
+    return n*2;
+}*/
+let fruits=["apple", "banana", "kiwi","mango","orange"];
+/*for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}*/
+/*fruits.push("papaya");
+for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
+fruits.pop();
+for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
+for(let val of fruits){
+    console.log(val)
+}*/
+let nums=[2,4,8,9]
+let sum=nums.reduce((acc,val)=>acc+val,0);
+console.log(sum);
